684d33ff7e084ced4de3ba5dcaa45230791c8f46
Mark Grennan <grennama@tcd.ie>
2018-10-23 22:19:30 +0100

    changed colors to hsv

--- setUp.py
+++ setUp.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from textAnalysis_ex import *
+import colorsys
 
 def main():
         # load the dataset
@@ -61,10 +62,10 @@ def main():
         data.drop (columns = ['gender'], inplace = True)
 
         # categorize colours
-        data['link_color_catg']=pd.factorize(data['link_color'])[0]
-        data['sidebar_color_catg']=pd.factorize(data['sidebar_color'])[0]
-        data.drop (columns = ['sidebar_color'], inplace = True)
-        data.drop (columns = ['link_color'], inplace = True)
+        # data['link_color_catg']=pd.factorize(data['link_color'])[0]
+        # data['sidebar_color_catg']=pd.factorize(data['sidebar_color'])[0]
+        # data.drop (columns = ['sidebar_color'], inplace = True)
+        # data.drop (columns = ['link_color'], inplace = True)
 
         # analyize text for sentiment & drop text column
         text_sent=[]
@@ -73,13 +74,64 @@ def main():
         data['text_sent']=text_sent
         data.drop(columns = ['text'], inplace=True)
 
+        # convert linkColor to hsv
+        H, S, V = [], [], []
+        linkColorList = data['link_color']
+        for color in linkColorList:
+            try:
+                color = list(int(color[i:i+2], 16) for i in (0, 2 ,4))
+            except ValueError:
+                color = [0, 0, 0]
+            # must divide by 255 as co-ordinates are in range 0 to 1
+            hsv = colorsys.rgb_to_hsv(color[0]/255, color[1]/255, color[2]/255)
+            # rescale to hsv
+            x = round(hsv[0]*360, 1)
+            y = round(hsv[1]*100, 1)
+            z = round(hsv[2]*100, 1)
+            H.append(x)
+            S.append(y)
+            V.append(z)
+
+        data['link_hue'] = H
+        data['link_sat'] = S
+        data['link_vue'] = V
+        data.drop (columns = ['link_color'], inplace = True)
+
+        # convert sidebar to hsv
+        H2, S2, V2 = [], [], []
+        sidebarColorList = data['sidebar_color']
+        for color in sidebarColorList:
+            try:
+                color = list(int(color[i:i+2], 16) for i in (0, 2 ,4))
+            except ValueError:
+                color = [0, 0, 0]
+            # must divide by 255 as co-ordinates are in range 0 to 1
+            hsv = colorsys.rgb_to_hsv(color[0]/255, color[1]/255, color[2]/255)
+
+            # rescale to hsv
+            x = round(hsv[0]*360, 1)
+            y = round(hsv[1]*100, 1)
+            z = round(hsv[2]*100, 1)
+            H2.append(x)
+            S2.append(y)
+            V2.append(z)
+
+        data['sidebar_hue'] = H2
+        data['sidebar_sat'] = S2
+        data['sidebar_vue'] = V2
+        data.drop (columns = ['sidebar_color'], inplace = True)
+
         # standardize numeric variables (could also consider using robust scaler here)
         numericVariables = ['fav_number', 'tweet_count','retweet_count', 'totalLettersName',
-         'year', 'month', 'link_color_catg', 'sidebar_color_catg']
+         'year', 'month', 'link_hue', 'link_vue', 'link_sat',
+         'sidebar_hue', 'sidebar_sat', 'sidebar_vue']
+
         scaler = preprocessing.StandardScaler()
         data[numericVariables] = scaler.fit_transform(data[numericVariables])
 
         data.to_csv('cleanData.csv')
+        data.info()
+        print (data.head(5))
 
 if __name__ == '__main__':
   main()
