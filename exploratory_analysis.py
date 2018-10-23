1545996d5e9c747e8808dcc5728e133754de55b1
Mark Grennan <grennama@tcd.ie>
2018-10-23 19:12:53 +0100

    see graphs

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -9,6 +9,12 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.feature_selection import RFECV
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import RFE
+import statsmodels.api as sm
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+
 def boxplot(column):
     # to do: return a boxplot of each variables
     return 0
@@ -47,7 +53,7 @@ def main():
     startTime = time.time()
 
     # load the dataset
-    dataset = '/home/markg/Documents/TCD/ML/ML1819--task-107--team-11/dataset/custom_color_dataset.csv'
+    dataset = '/home/markg/Documents/TCD/ML/ML1819--task-107--team-11/dataset/overall_dataset.csv'
     data = pd.read_csv(dataset, na_values = '?')
 
     # change appropriate variables to categorical. DON'T DO THIS!
@@ -73,43 +79,54 @@ def main():
 
     ##################### END SETUP CODE ######################################
 
-    # create dependent & independent variables
-    X = data.drop('gender', axis=1)
-    y = data['gender']
-
-    # split into 90% training, 10% testing
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10)
-
-    # train model (could change kernel here)
-    svm = SVC(C=1, gamma=0.3, kernel='rbf')
-    svm.fit(X_train, y_train)
-
-    # recursive feature selection using cross validation
-    # rfecv = RFECV(estimator=svm, step=1, cv=StratifiedKFold(2),
-    #               scoring='accuracy')
-    # rfecv.fit(X, y)
-    # print("Optimal number of features : %d" % rfecv.n_features_)
-    # print("Feature ranking: ", rfecv.ranking_)
+    #################### SVM MODEL ############################################
+    # # create dependent & independent variables
+    # X = data.drop(['gender', 'fav_number', 'user_timezone', 'tweet_count','retweet_count', 'link_hue',
+    #  'link_sat', 'link_vue', 'sidebar_sat', 'sidebar_vue', 'month'], axis=1)
+    # # X = data.drop('gender', axis=1)
+    # y = data['gender']
+    # # print (X.keys())
     #
-    # # plot bar chart of feature ranking
-    # features = list(X)
-    # ranking = rfecv.ranking_
-    # plt.bar(features, ranking, align='center', alpha=0.5)
-    # plt.show()
     #
-    # # Plot number of features VS. cross-validation scores
-    # plt.figure()
-    # plt.xlabel("Number of features selected")
-    # plt.ylabel("Cross validation score (nb of correct classifications)")
-    # plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
-    # plt.show()
+    # # split into 90% training, 10% testing
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10)
+    #
+    # # # train model (could change kernel here)
+    # svm = SVC(C=1, gamma=0.3, kernel='rbf')
+    # svm.fit(X_train, y_train)
+    # #
+    # # # # recursive feature selection using cross validation
+    # # # rfecv = RFECV(estimator=svm, step=1, cv=StratifiedKFold(2),
+    # # #               scoring='accuracy')
+    # # # rfecv.fit(X, y)
+    # # # print("Optimal number of features : %d" % rfecv.n_features_)
+    # # # print("Feature ranking: ", rfecv.ranking_)
 
-    # # make predictions and print metrics
+    # # # recursive feature selection without cross validation
+    # rfe = RFE(svm, 3)
+    # fit = rfe.fit(X, y)
+    # print('Num Features:',fit.n_features_to_select)
+    # print("Selected Features:",fit.support_)
+    # #
+    # plot bar chart of feature ranking
+    features = list(X)
+    ranking = rfecv.ranking_
+    plt.bar(features, ranking, align='center', alpha=0.5)
+    plt.show()
+
+    # Plot number of features VS. cross-validation scores
+    plt.figure()
+    plt.xlabel("Number of features selected")
+    plt.ylabel("Cross validation score (nb of correct classifications)")
+    plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+    plt.show()
+    #
+    # make predictions and print metrics
     y_pred = svm.predict(X_test)
     print(classification_report(y_test,y_pred))
     print(confusion_matrix(y_test,y_pred))
-
-    # # cross validation to choose c and gamma
+    # #
+    # # # # cross validation to choose c and gamma
     # C_s, gamma_s = np.meshgrid(np.logspace(-2, 1, 20), np.logspace(-2, 1, 20))
     # scores = list()
     # i=0; j=0
@@ -126,6 +143,8 @@ def main():
     # ax2.set_ylabel('gamma')
     # fig2.colorbar(c)
     # fig2.savefig('crossvalOverall.png')
+
+    ################## END SVM MODEL ##########################################
 
 #     # create a subset of males and females
 # #     males = data[data['gender']==0]
@@ -147,6 +166,41 @@ def main():
 #     #plot_hist(fav_number, "title", "favourited tweets", "freq")
 #
 
+    #################### LOGISTIC MODEL #######################################
+    # create dependent & independent variables
+    # X = data.drop('gender', axis=1)
+    # Y = data['gender']
+    #
+    # model = LogisticRegression()
+    # rfe = RFE(model, 3)
+    # fit = rfe.fit(X, Y)
+    # print('Num Features:',fit.n_features_to_select)
+    # print("Selected Features:",fit.support_)
+    #
+    # # build model
+    # logit_model=sm.Logit(Y,X)
+    # result=logit_model.fit()
+    #
+    # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
+    # logreg = LogisticRegression()
+    # logreg.fit(X_train, y_train)
+    #
+    # y_pred = logreg.predict(X_test)
+    # # print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+    # print(classification_report(y_test,y_pred))
+    # # logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+    # # fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+    # # plt.figure()
+    # # plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+    # # plt.plot([0, 1], [0, 1],'r--')
+    # # plt.xlim([0.0, 1.0])
+    # # plt.ylim([0.0, 1.05])
+    # # plt.xlabel('False Positive Rate')
+    # # plt.ylabel('True Positive Rate')
+    # # plt.title('Receiver operating characteristic')
+    # # plt.savefig('Log_ROC')
+    # # plt.show()
+
     # to keep track of time taken
     endTIme = time.time()
     totalTime = endTIme - startTime
@@ -154,15 +208,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
-#
-# see notes on repl
-# to do: compute decision tree with chosen dependent variables
-# return: recall precision and f1 score for decision tree
-#
-# to do: compute logisitic regression
-# return: recall precision and f1
-# http://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html.
-#
-# to do: compute SVM
-# return: recall precision and f1
